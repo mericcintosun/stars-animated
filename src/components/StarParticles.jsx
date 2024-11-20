71992d95da0ebe380c8b67fcfa9eaca4b36ce089
mericcintosun <mericcintosunn@gmail.com>
2024-11-20 19:23:38 +0300

    added

--- src/components/StarParticles.jsx
+++ src/components/StarParticles.jsx
@@ -52,7 +52,7 @@ const StarParticles = () => {
     },
     particles: {
       number: {
-        value: 400, // Parçacık sayısı
+        value: 300, // Parçacık sayısı
         density: {
           enable: true,
           width: 1920, // Yoğunluk genişliği
